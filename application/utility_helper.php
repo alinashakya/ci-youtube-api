a458a3ca4ce23a944d558fc2990d747950233e92
Alina Shakya <alina.shakya3@gmail.com>
2017-11-05 19:43:57 +0000

    #master - adds validations and manages configurations

---
+++ application/utility_helper.php
@@ -0,0 +1,5 @@
+<?php
+function asset_url(){
+	echo 'dsad';die;
+   //return base_url().'assets/';
+}
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -5,10 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to Youtube API</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script type="text/javascript" src="js/jquery-2.0.0.js"></script>
-	<script type="text/javascript" src="js/search.js"></script>
+	<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/style.css">
+	<link rel="stylesheet" href="<?php echo asset_url(); ?>/css/bootstrap.min.css">
+	<script type="text/javascript" src="<?php echo asset_url(); ?>/js/jquery-2.0.0.js"></script>
+	<script type="text/javascript" src="<?php echo asset_url(); ?>/js/search.js"></script>
 	<script type="text/javascript">
 		window.onload = function(e){ 
 			document.searchForm.action = "";
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-<?php 
+<?php
 echo form_open('/',array('id' => 'search-form','name'=>'searchForm')); ?>
 <?php  
 echo form_input(array('id' => 'sname', 'name' => 'sname', 'type' => 'search', 'placeholder' => 'Youtube Search', 'class' => 'input-append'));
